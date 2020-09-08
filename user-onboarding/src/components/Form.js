14d72f32ec29875f8a14fcefef5fc68047c44d08
Mychael M <mychaelbmenges@gmail.com>
2020-09-08 17:43:02 -0600

    added form schema using yup

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,4 +1,15 @@
 import React, { useState } from 'react';
+import * as yup from 'yup';
+
+const formSchema = yup.object().shape({
+  name: yup.string().required("Name is definitely required."),
+  email: yup
+        .string()
+        .email("This Must be an Email Address")
+        .required("You have to provide an Email Address"),
+  password: yup.string().length(8, 'Password must be at least 8 characters'),
+  terms: yup.boolean().oneOf([true], "Agree to the Terms mate")
+})
 
 function Form() {
 
@@ -14,16 +25,23 @@ function Form() {
     // console.log(e.target.value);
     let value = e.target.type === "checkbox" ? e.target.checked : e.target.value;
     setFormData({ ...formData, [e.target.name]: value })
-    console.log(formData);
+    // console.log(formData);
+  }
+
+  const formSubmit = e => {
+    e.preventDefault();
+    console.log('Submitted Form');
+
   }
   
   return (
-    <form>
+    <form onSubmit={formSubmit}>
       <label htmlFor="name">Name
         <input 
           type="text"
           name='name'
           id='name'
+          value={formData.name}
           onChange={inputChange}
         />
       </label>
@@ -34,6 +52,7 @@ function Form() {
           type="email"
           name='email'
           id='email'
+          value={formData.email}
           onChange={inputChange}
         />
       </label>
@@ -44,6 +63,7 @@ function Form() {
           type="password"
           name='password'
           id='password'
+          value={formData.password}
           onChange={inputChange}
         />
       </label>
@@ -54,6 +74,7 @@ function Form() {
           type="checkbox"
           name='terms'
           id='terms'
+          checked={formData.terms}
           onChange={inputChange}
         />
       </label>
