1c9e051e40e3fdb97c2f5c60ede5bc678934216e
Mychael M <mychaelbmenges@gmail.com>
2020-09-08 17:09:20 -0600

    created basic form in Form.js

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -3,7 +3,45 @@ import React from 'react';
 function Form() {
   
   return (
-    <p>I'm here</p>
+    <form>
+      <label htmlFor="name">Name
+        <input 
+          type="text"
+          name='name'
+          id='name'
+        />
+      </label>
+      <br/>
+
+      <label htmlFor="email">Email
+        <input 
+          type="email"
+          name='email'
+          id='email'
+        />
+      </label>
+      <br />
+
+      <label htmlFor="password">Password
+        <input 
+          type="password"
+          name='password'
+          id='password'
+        />
+      </label>
+      <br />
+
+      <label htmlFor="name">Terms & Service
+        <input 
+          type="checkbox"
+          name='terms'
+          id='terms'
+        />
+      </label>
+      <br />
+
+      <button type="submit">Submit</button>
+    </form>
   )
 }
 
