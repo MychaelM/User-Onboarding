d2329aa12632105a3c8160cd483484f230ba53c7
Mychael M <mychaelbmenges@gmail.com>
2020-09-10 17:41:48 -0600

    fixedc issue with checkbox validation

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -9,7 +9,7 @@ const formSchema = yup.object().shape({
         .email("This Must be an Email Address")
         .required("You have to provide an Email Address"),
   password: yup.string().min(5, 'Password must be at least 5 characters'),
-  terms: yup.bool().oneOf([true], "Agree to the Terms mate")
+  terms: yup.boolean().oneOf([true], "Agree to the Terms mate")
 })
 
 function Form() {
@@ -35,21 +35,25 @@ function Form() {
   // }, [users]);
 
   const validate = e => {
+    let value = e.target.type === "checkbox" ? e.target.checked : e.target.value;
 
     yup
       .reach(formSchema, e.target.name)
-      .validate(e.target.value)
+      .validate(value)
       .then(valid => {
         setErrorState({
           ...errorState, 
           [e.target.name]: ''
         });
+        console.log(errorState);
       })
       .catch(err => {
+        let error = err.errors[0];
         setErrorState({
           ...errorState,
-          [e.target.name]: err.errors[0]
+          [e.target.name]: error
         });
+        console.log(errorState);
       })
   }
 
@@ -60,6 +64,7 @@ function Form() {
     validate(e);
     let value = e.target.type === "checkbox" ? e.target.checked : e.target.value;
     setFormData({ ...formData, [e.target.name]: value })
+    // console.log(e.target.type);
     // console.log(formData);
   }
 
@@ -86,69 +91,76 @@ function Form() {
   
   return (
     <div>
-    <form onSubmit={formSubmit}>
-      <label htmlFor="name">Name
-        <input 
-          type="text"
-          name='name'
-          id='name'
-          value={formData.name}
-          onChange={inputChange}
-        />
-      </label>
-      <br/>
-      {errorState.name.length > 0 ? (
-        <p className="nameError">{errorState.name}</p>
-      ) : null}
-
-      <label htmlFor="email">Email
-        <input 
-          type="email"
-          name='email'
-          id='email'
-          value={formData.email}
-          onChange={inputChange}
-        />
-      </label>
-      <br />
-      {errorState.email.length > 0 ? (
-        <p>{errorState.email}</p>
-      ) : null}
-
-      <label htmlFor="password">Password
-        <input 
-          type="password"
-          name='password'
-          id='password'
-          value={formData.password}
-          onChange={inputChange}
-        />
-      </label>
-      <br />
-      {errorState.password.length > 0 ? (
-        <p>{errorState.password}</p>
-      ) : null}
-
-      <label htmlFor="name">Terms & Service
-        <input 
-          type="checkbox"
-          name='terms'
-          id='terms'
-          checked={formData.terms}
-          onChange={inputChange}
-        />
-        {errorState.terms > 0 ? (
-          <p>{errorState.terms}</p>
+      <form onSubmit={formSubmit}>
+        <label htmlFor="name">
+          Name
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={formData.name}
+            onChange={inputChange}
+          />
+        </label>
+        <br />
+        {errorState.name.length > 0 ? (
+          <p className="nameError">{errorState.name}</p>
         ) : null}
-      </label>
-      <br />
 
-      <button data-cy="submitButton" type="submit">Submit</button>
-    </form>
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={formData.email}
+            onChange={inputChange}
+          />
+        </label>
+        <br />
+        {errorState.email.length > 0 ? <p>{errorState.email}</p> : null}
 
-      {users.map((user, index) => (<pre key={index}>{JSON.stringify(user.data)}</pre>))}
+        <label htmlFor="password">
+          Password
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={formData.password}
+            onChange={inputChange}
+          />
+        </label>
+        <br />
+        {errorState.password.length > 0 ? <p>{errorState.password}</p> : null}
+
+        <label htmlFor="name">
+          Terms & Service
+          <input
+            type="checkbox"
+            name="terms"
+            id="terms"
+            checked={formData.terms}
+            onChange={inputChange}
+          />
+          {errorState.terms.length > 0 ? (
+            <p className="error">{errorState.terms}</p>
+          ) : null}
+        </label>
+        {/* {errorState.terms > 0 ? (
+          <p className="error">{errorState.terms}</p>
+        ) : null} */}
+        <br />
+
+        <button data-cy="submitButton" type="submit">
+          Submit
+        </button>
+      </form>
+
+      {users.map((user, index) => (
+        <pre key={index}>{JSON.stringify(user.data)}</pre>
+      ))}
     </div>
-  )
+  );
 }
 
 export default Form;
